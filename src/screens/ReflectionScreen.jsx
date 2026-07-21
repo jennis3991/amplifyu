9eaf1f892064ecbb628dd17997eec89c64f29c58
jennis3991 <j.singh.05@aberdeen.ac.uk>
2026-07-21 20:09:19 +0100

    feat: chess queen banner on desktop communication profile

--- src/screens/ReflectionScreen.jsx
+++ src/screens/ReflectionScreen.jsx
@@ -131,7 +131,7 @@ export function ReflectionScreen({ answers, onContinue }) {
 
           {/* Banner image */}
           <div style={{ width: "100%", height: 380, flexShrink: 0, overflow: "hidden" }}>
-            <img src="/profile-banner-desktop.jpg" alt="" style={{ width: "100%", height: "100%", display: "block", objectFit: "cover", objectPosition: "left top" }}/>
+            <img src="/profile-banner-desktop-2.jpg" alt="" style={{ width: "100%", height: "100%", display: "block", objectFit: "cover", objectPosition: "center" }}/>
           </div>
 
           <div style={{ padding: "52px 72px 120px", display: "flex", flexDirection: "column" }}>
